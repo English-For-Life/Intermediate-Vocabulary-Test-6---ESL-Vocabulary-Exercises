c5f1559947e03058f8fcaaa1d02a9137258cc23a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 23:21:36 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"He's such a kind, ............... man; so popular with his workmates.",
+        options:["generous", "wicked and mean", "aimless", "hostile", "outrageous"],
+        answer: 0
+    },
+    {
+        q:"Food kept too long outside a fridge will ............... through the action of moulds and bacteria on it.",
+        options:["pull apart", "go off", "be out", "throw out", "turn down"],
+        answer: 1
+    },
+    {
+        q:"She is the ............... daughter of a prominent Turkish politician. Her real parents had been killed in a car crash when she was little.",
+        options:["saved", "adapted", "adopted", "refused", "accustomed"],
+        answer: 2
+    },
+    {
+        q:"People ............... for all sorts of reasons, but a desire to slim down tops the list.",
+        options:["fit into", "run out", "dine", "diet", "fight"],
+        answer: 3
+    },
+    {
+        q:"Do be ............... , sweetheart. All the shops are closed at this hour.",
+        options:["limited", "embarrassed", "essential", "exceptional", "reasonable"],
+        answer: 4
+    },
+    {
+        q:"All pines are ............... trees with needlelike leaves.",
+        options:["evergreen", "permanent", "lasting", "enormous", "everlasting"],
+        answer: 0
+    },
+    {
+     q:"She is terrified of caves. So, she is ............... to come with us.",
+        options:["dying", "unwilling", "curious", "eager", "intent"],
+        answer: 1   
+    },
+    {
+        q:"Although it is a fairly small town, there are numerous public ............... , such as meeting halls, art museums, swimmimg pools, and sports parks.",
+        options:["hindrances", "capacities", "facilities", "obstacles", "shelters"],
+        answer: 2
+    },
+    {
+        q:"A............... man smiles in the face of misfortune.",
+        options:["serious", "funny", "unselfish", "courageous", "absolute"],
+        answer: 3
+    },
+    {
+        q:"You remember the day she left the town? Do you ............... what she was wearing.",
+        options:["notice", "remind", "observe", "predict", "recall"],
+        answer: 4
+    }
+]
